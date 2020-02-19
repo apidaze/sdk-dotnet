17f84bac2233e1a6cab6b7b12749657969950c0b
bednarz1990 <lk.bednarczyk@gmail.com>
2020-02-19 11:52:50 +0100

    Refactor code and clean.

--- Apidaze.SDK.Tests.Integration/IvrExample/WebService.cs
+++ Apidaze.SDK.Tests.Integration/IvrExample/WebService.cs
@@ -18,7 +18,7 @@ namespace IvrExample
         /// <summary>
         /// The port the HttpListener should listen on.
         /// </summary>
-        private static readonly string SERVER_URL = "http://.ngrok.io";
+        private static readonly string SERVER_URL = "";
 
         /// <summary>
         /// The localhost.
